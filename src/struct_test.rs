c530609f1abe0ddfe28f948f7000a01d325f9eea
david l euler <suibwx@163.com>
2022-11-21 10:24:36 +0800

    Create struct_test.rs

---
+++ src/struct_test.rs
@@ -0,0 +1,4 @@
+//One difference between Rust and C++ is that data and behaviour are much more strictly separated in Rust than C++ (or Java, or other OO languages).
+// Behaviour is defined by functions and those can be defined in traits and impls (implementations), 
+// but traits cannot contain data, they are similar to Java's interfaces in that respect.
+
