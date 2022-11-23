f8c9aa0636495ef32f0769f0be125ebbd857673b
david l euler <suibwx@163.com>
2022-11-23 15:20:26 +0800

    Update string_test.rs

--- src/bin/string_test.rs
+++ src/bin/string_test.rs
@@ -4,4 +4,9 @@ fn main() {
     // parse string to i64:
     let i = "123".parse::<i64>();
     println!("{:?}", i);
+    
+    // concatenate two string:
+    let world = String::from( "world");
+    let a = r2.to_owned() + &world;
+    println!("a={a}");
 }
